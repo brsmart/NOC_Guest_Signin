1a02f7bc79890fdeae469e5223850e09d03ef75c
Brian Smart <bnksmart@hotmail.com>
2013-11-15 14:45:58 -0700

    added visit show styling

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,11 @@
 module ApplicationHelper
-  def format_date(date)
-    return date.localtime.strftime("%02m/%02d/%04Y @ %02H:%02M") unless date.nil?
+
+  def format_date( date )
+    return date.localtime.strftime( "%m.%d.%y @ %H:%M" ) unless date.nil?
 
     ""
   end
+
   def current_user_name
     user = User.find(current_user.id)
     user["email"]
